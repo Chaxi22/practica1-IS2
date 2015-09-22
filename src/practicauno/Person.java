ea9041c8db7fc072cd49803fc01f1426d12f57d4
Chaxi <Chaxi@192.168.1.13>
2015-09-22 16:21:31 +0100

    Segunda version

--- src/practicauno/Person.java
+++ src/practicauno/Person.java
@@ -15,6 +15,7 @@ public class Person {
     private final String name;
     private final String surname;
     private final Date birthday;
+    private static final long MILLISECONDS_PER_YEAR = (long) (1000*60*60*24*365.25);
 
     public Person(String name, String surname, Date birthday) {
         this.name = name;
@@ -40,6 +41,10 @@ public class Person {
     
     public int getAge(){
         Date today = new Date();
-        return (int)((today.getTime() - birthday.getTime())/(1000*60*60*24*365.25));
+        return (int) miMetodo(today.getTime()-birthday.getTime());
     }
+    
+    private long miMetodo(long millis){
+       return millis/MILLISECONDS_PER_YEAR;
+    } 
 }
